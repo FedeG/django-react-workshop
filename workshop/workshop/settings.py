843479859252b4505f785013337a3496edcf68bb
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-10-19 18:41:33 -0300

    Added templates folder to settings

--- workshop/workshop/settings.py
+++ workshop/workshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'workshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'links/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
