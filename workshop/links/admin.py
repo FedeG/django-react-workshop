71c9fb026a6cd2f5b4e7ddfce9e2f42e237771de
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-10-21 00:49:25 -0300

    Added new models to Django admin

--- workshop/links/admin.py
+++ workshop/links/admin.py
@@ -1,3 +1,22 @@
+"""
 from django.contrib import admin
 
 # Register your models here.
+"""
+
+from django.contrib import admin
+from .models import Link, Tag, LinkTag
+
+
+class LinkTagInline(admin.TabularInline):
+    model = LinkTag
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    inlines = [LinkTagInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
