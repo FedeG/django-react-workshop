ff5085645328d6458f6b5be5faaf04a3c4ba7573
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-10-27 17:07:19 -0300

    Added app index with 'Sample App!' in render

--- workshop/front/src/components/App/index.jsx
+++ workshop/front/src/components/App/index.jsx
@@ -8,7 +8,7 @@ export default class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Headline>Something New!</Headline>
+            <Headline>Sample App!</Headline>
           </div>
         </div>
       </div>
