38c3253c11780a7ccccf3719dd9e83d385cc17ff
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-07 19:03:13 -0300

    Add linkdetail view to urls

--- workshop/links/urls.py
+++ workshop/links/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.views import generic
+from . import views
 
 urlpatterns = [
     url(r'^view2/',
         generic.TemplateView.as_view(template_name='view2.html')),
-    url(r'^$',
-        generic.TemplateView.as_view(template_name='view1.html')),
+    url(r'^$', views.links_detail),
 ]
