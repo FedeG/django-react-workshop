0f73d344c8cc9405a3bb6ec9bc14f7e56af4ffb5
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-07 18:06:26 -0300

    Fixed tests

--- workshop/front/src/components/App/App.spec.jsx
+++ workshop/front/src/components/App/App.spec.jsx
@@ -7,7 +7,7 @@ describe('App Component', () => {
   describe('#render', () => {
 
     it('should render the component properly', () => {
-      const wrapper = shallow(<App id="id" audioPath="path"/>);
+      const wrapper = shallow(<App/>);
       const componentInDOM = '<div class="container"><div class="row"><div class="col-sm-12"><h1>Sample App!</h1></div></div></div>';
       expect(wrapper.html()).toBe(componentInDOM);
     })
