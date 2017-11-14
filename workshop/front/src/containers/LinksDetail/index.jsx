8450ecf525cb32317b8f3532d256acf21851e845
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-14 12:25:30 -0300

    Create button component

--- workshop/front/src/containers/LinksDetail/index.jsx
+++ workshop/front/src/containers/LinksDetail/index.jsx
@@ -2,16 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 import LinksDetailComponent from '../../components/LinksDetail'
+import { LINKS_API_URL } from '../../utils/urls'
+import { getUrl } from '../../utils/api'
+
 
 export default class LinksDetail extends React.Component {
   static propTypes = {
-    links: PropTypes.array
+    links: PropTypes.array.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    const { links } = this.props;
+    this.state = {
+      links: [...links]
+    }
+  }
+
+
+  _onRefresh = () => {
+    getUrl(LINKS_API_URL)
+      .then(newLinks => {
+        const links = newLinks.map(link => {
+          return {
+            pk: link.id,
+            fields: link
+          }
+        });
+        this.setState({links});
+      })
   }
 
   render() {
-    const { links } = this.props;
+    const { links } = this.state;
     return (
-      <LinksDetailComponent links={links} />
+      <LinksDetailComponent links={links} onRefresh={this._onRefresh}/>
     )
   }
 }
