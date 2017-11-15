a844dfa05c75c262b08398f8dd57e04ff25356f7
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-15 02:09:44 -0300

    Add websockert to linksDetail

--- workshop/front/src/containers/LinksDetail/index.jsx
+++ workshop/front/src/containers/LinksDetail/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import Websocket from 'react-websocket';
 
 import LinksDetailComponent from '../../components/LinksDetail'
-import { LINKS_API_URL } from '../../utils/urls'
+import { LINKS_API_URL, LINKS_WS_URL } from '../../utils/urls'
 import { getUrl } from '../../utils/api'
 
 
@@ -19,24 +20,44 @@ export default class LinksDetail extends React.Component {
     }
   }
 
+  getLink = (id, fields) => {return {id, fields}};
 
   _onRefresh = () => {
     getUrl(LINKS_API_URL)
       .then(newLinks => {
-        const links = newLinks.map(link => {
-          return {
-            pk: link.id,
-            fields: link
-          }
-        });
+        const links = newLinks.map(link => this.getLink(link.id, link));
         this.setState({links});
       })
+  }
+
+  _onUpdate = event => {
+    const { links } = this.state;
+    const {payload: {action, data, pk}} = JSON.parse(event);
+    let newLinks = [...links];
+    switch (action) {
+      case 'update':
+        newLinks = newLinks.map(link => {
+          if (link.pk === pk) return this.getLink(pk, data);
+          return link;
+        })
+        break;
+      case 'create':
+        newLinks.push(this.getLink(pk, data))
+        break;
+     case 'delete':
+        newLinks = newLinks.filter(link => link.pk !== pk);
+        break;
+    }
+    this.setState({links: newLinks});
   }
 
   render() {
     const { links } = this.state;
     return (
-      <LinksDetailComponent links={links} onRefresh={this._onRefresh}/>
+      <div>
+        <LinksDetailComponent links={links} onRefresh={this._onRefresh}/>
+        <Websocket url={LINKS_WS_URL} onMessage={this._onUpdate}/>
+      </div>
     )
   }
 }
