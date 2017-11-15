f765465befe7fe1e93644e235a2b0eeb0db738be
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-15 02:15:31 -0300

    Add tests for new urls in utils

--- workshop/front/src/utils/urls.spec.js
+++ workshop/front/src/utils/urls.spec.js
@@ -1,4 +1,4 @@
-import { API_URL, LINKS_API_URL } from './urls.js';
+import { API_URL, LINKS_API_URL, WS_URL, LINKS_WS_URL } from './urls.js';
 
 describe('Url utils', () => {
 
@@ -14,6 +14,22 @@ describe('Url utils', () => {
 
     it('should LINKS_API_URL is /links/api/links/', () => {
       expect(LINKS_API_URL).toEqual('/links/api/links/');
+    })
+
+  })
+
+  describe('WS_URL', () => {
+
+    it('should is WS_URL is ws://localhost:5000/', () => {
+      expect(WS_URL).toEqual('ws://localhost:5000/');
+    })
+
+  })
+
+  describe('LINKS_WS_URL', () => {
+
+    it('should is LINKS_WS_URL is ws://localhost:5000/update/links/', () => {
+      expect(LINKS_WS_URL).toEqual('ws://localhost:5000/update/links/');
     })
 
   })
