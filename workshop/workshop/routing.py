011fa0a36ae3c52bbf22626578e4c3f0f8862f07
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-15 01:46:44 -0300

    Add routes for bindings

---
+++ workshop/workshop/routing.py
@@ -0,0 +1,50 @@
+from channels.generic.websockets import WebsocketDemultiplexer
+from channels.routing import route, route_class
+
+from links.bindings import LinkBinding, TagBinding, LinkTagBinding
+
+
+class APIDemultiplexer(WebsocketDemultiplexer):
+    consumers = {
+        'links': LinkBinding.consumer,
+        'tags': TagBinding.consumer,
+        'linktags': LinkTagBinding.consumer,
+    }
+
+    def connection_groups(self, **kwargs):
+        return ['link-updates', 'tag-updates', 'linktag-updates']
+
+
+class LinkTagDemultiplexer(WebsocketDemultiplexer):
+    consumers = {
+        'linktags': LinkTagBinding.consumer
+    }
+
+    def connection_groups(self, **kwargs):
+        return ['linktag-updates']
+
+
+class LinkDemultiplexer(WebsocketDemultiplexer):
+    consumers = {
+        'links': LinkBinding.consumer
+    }
+
+    def connection_groups(self, **kwargs):
+        return ['link-updates']
+
+
+class TagDemultiplexer(WebsocketDemultiplexer):
+    consumers = {
+        'tags': TagBinding.consumer
+    }
+
+    def connection_groups(self, **kwargs):
+        return ['tag-updates']
+
+
+channel_routing = [
+    route_class(APIDemultiplexer, path='^/updates/$'),
+    route_class(LinkTagDemultiplexer, path='^/updates/linktags/$'),
+    route_class(LinkDemultiplexer, path='^/updates/links/$'),
+    route_class(TagDemultiplexer, path='^/updates/tags/$'),
+]
