de9bc4f8a9163eb3d52df5cef92fdd83c45daa9d
FedeG <federicogonzalez761@gmail.com>
2020-03-29 17:40:14 -0300

    Update models and admin (add status, add similars, add stars, ...)

--- workshop/links/admin.py
+++ workshop/links/admin.py
@@ -3,6 +3,9 @@
 """
 
 from django.contrib import admin
+from django.db import IntegrityError
+from django.utils.html import format_html
+
 from .models import Link, Tag, LinkTag
 
 
@@ -19,6 +22,29 @@ class LinkAdmin(admin.ModelAdmin):
         Link model registration in Django admin
     """
     inlines = [LinkTagInline]
+    list_filter = ('status', 'pending', 'tags', 'stars',)
+    list_display = ('name', 'status', 'stars', 'pending', 'get_tags', 'link',)
+
+    def get_tags(self, instance):
+        return '\n'.join([tag.name for tag in instance.tags.all()])
+
+    def link(self, instance):
+        return format_html('<a href="{}">{}</a>'.format(instance.url, instance.url))
+    link.allow_tags = True
+
+
+def merge_similars(modeladmin, request, queryset):
+    for tag in queryset:
+        similars = tag.get_similars()
+        for similar in similars:
+            for link_tag in similar.linktag_set.all():
+                try:
+                    new_link_tag = LinkTag.objects.create(link=link_tag.link, tag=tag)
+                    new_link_tag.save()
+                except IntegrityError:
+                    pass
+                link_tag.delete()
+merge_similars.short_description = "Merge similars tags"
 
 
 @admin.register(Tag)
@@ -26,4 +52,5 @@ class TagAdmin(admin.ModelAdmin):
     """
         Tag model registration in Django admin
     """
-    pass
+    list_display = ('name', 'links_quantity', 'unused', 'similars', 'description',)
+    actions = [merge_similars]
--- workshop/links/models.py
+++ workshop/links/models.py
@@ -4,6 +4,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.translation import ugettext as _
 from links.utils import is_similar
 
@@ -12,18 +13,30 @@ class Tag(models.Model):
     """
         Tag model
     """
-    name = models.CharField(_('name'), max_length=30)
+    name = models.CharField(_('name'), max_length=30, unique=True)
     description = models.TextField(_('description'), blank=True, null=True)
     user = models.ForeignKey(
         User, verbose_name=_('user'), blank=True, null=True,
         on_delete=models.CASCADE)
+
+    @property
+    def links_quantity(self):
+        return self.linktag_set.all().count()
+
+    @property
+    def similars(self):
+        return self.get_similars()
+
+    @property
+    def unused(self):
+        return self.links_quantity == 0
 
     def __str__(self):
         return self.name
 
     def get_similars(self):
         """
-            Return similars links (search for name)
+            Return similars tags (search for name)
         """
         tags = Tag.objects.all().exclude(pk=self.pk)
         similars = [tag for tag in tags if is_similar(self.name, tag.name)]
@@ -38,16 +51,36 @@ class Link(models.Model):
     """
         Link model
     """
+    ONLINE = 'ON'
+    OFFLINE = 'OFF'
+    CHECK = 'NEW'
+    ERROR = 'ERR'
+    STATUS_CHOICES = [
+        (ONLINE, 'Online'),
+        (OFFLINE, 'Offline'),
+        (ERROR, 'Error'),
+        (CHECK, 'Check'),
+    ]
+    status = models.CharField(
+        max_length=3,
+        choices=STATUS_CHOICES,
+        default=CHECK,
+    )
     name = models.CharField(_('name'), max_length=30)
-    url = models.URLField(_('url'))
-    pending = models.BooleanField(_('pending'), default=False)
+    url = models.URLField(_('url'), unique=True)
+    pending = models.BooleanField(_('pending'), default=True)
     description = models.TextField(_('description'), blank=True, null=True)
     tags = models.ManyToManyField(Tag, through='LinkTag', editable=True)
     user = models.ForeignKey(User, verbose_name=_('user'),
                              on_delete=models.CASCADE)
+    stars = models.IntegerField(
+        _('stars'),
+        default=0,
+        validators=[MaxValueValidator(10), MinValueValidator(0)]
+    )
 
     def __str__(self):
-        return self.name
+        return '{}({}): {}'.format(self.name, self.status, self.url)
 
     class Meta:
         verbose_name = _('link')
